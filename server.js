0a678f81b8951b358e3b2133397d6f053a693143
Zakharov Viacheslav <zakharovvu@gmail.com>
2019-04-08 13:18:06 +0300

    init

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
 let port = process.env.PORT || 3000
-
 let data = { count: 0 };
+
+app.get('/', function (req, res) { 
+  res.set('Access-Control-Allow-Origin', '*')
+  res.send(JSON.stringify(data.count))
+})
+
 app.get('/inc', function (req, res) { 
   data.count++;
   res.set('Access-Control-Allow-Origin', '*')
