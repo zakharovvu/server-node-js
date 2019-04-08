4f93c6bb923b89c870bc1cd1ac4a87cf20eaa2e6
Zakharov Viacheslav <zakharovvu@gmail.com>
2019-04-08 12:12:16 +0300

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+
+const h1 = document.getElementById('h1')
+const inc = document.getElementById('inc')
+const dec = document.getElementById('dec')
+
+inc.addEventListener('click', () => {
+  increase.addEventListener('click', () => {
+    fetch('https://zakharov-test.herokuapp.com/inc')
+      .then(response => response.json())
+      .then(data => h1.textContent = `Count: ${data}`)
+  });
+})
+
+dec.addEventListener('click', () => {
+  increase.addEventListener('click', () => {
+    fetch('https://zakharov-test.herokuapp.com/dec')
+      .then(response => response.json())
+      .then(data => h1.textContent = `Count: ${data}`)
+  });
+})
