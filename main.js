1f57b6731ca4329f6500460d9ff69d50ae9ba6a4
Zakharov Viacheslav <zakharovvu@gmail.com>
2019-04-08 13:29:47 +0300

    init

--- main.js
+++ main.js
@@ -6,24 +6,24 @@ const reset = document.getElementById('reset')
 
 fetch('https://zakharov-test.herokuapp.com/inc')
       .then(response => response.json())
-      .then(data => h1.innerHTML = `Count: ${data}`)
+      .then(data => h1.textContent = `Count: ${data}`)
 
 
 inc.addEventListener('click', () => {
     fetch('https://zakharov-test.herokuapp.com/inc')
       .then(response => response.json())
-      .then(data => h1.innerHTML = `Count: ${data}`)
+      .then(data => h1.textContent = `Count: ${data}`)
 })
 
 dec.addEventListener('click', () => {
     fetch('https://zakharov-test.herokuapp.com/dec')
       .then(response => response.json())
-      .then(data => h1.innerHTML = `Count: ${data}`)
+      .then(data => h1.textContent = `Count: ${data}`)
 })
 
 reset.addEventListener('click', () => {
   fetch('https://zakharov-test.herokuapp.com/reset')
     .then(response => response.json())
-    .then(data => h1.innerHTML = `Count: ${data}`)
+    .then(data => h1.textContent = `Count: ${data}`)
 })
 
